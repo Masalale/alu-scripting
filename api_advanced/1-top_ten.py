dea9b39afab275a59cbb8e5fdeb3837e7fc44bd1
Chomba <clarenchomba59@gmail.com>
2025-07-19 15:04:35 +0000

    Refactor top_ten function for improved readability by removing unnecessary blank lines

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,15 +7,15 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     if not subreddit or not isinstance(subreddit, str):
         return
-    
+
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'python:subreddit.top.ten:v1.0 (by /u/pythonbot)'}
-    
+
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code != 200:
             return
-        
+
         data = response.json()
         posts = data.get('data', {}).get('children', [])
         for post in posts:
