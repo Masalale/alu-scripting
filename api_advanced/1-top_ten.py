da16d30993beea331dd01275cdf301d7e9efc675
Chomba <clarenchomba59@gmail.com>
2025-07-19 15:51:23 +0000

    Refactor top_ten function to improve error handling and simplify code structure

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,25 +4,13 @@ import requests
 
 
 def top_ten(subreddit):
-    if subreddit is None or not isinstance(subreddit, str):
-        print("None")
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
         return
-    
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {
-        'User-Agent': 'MyRedditApp/1.0'
-    }
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            
-            for i in range(min(10, len(posts))):
-                print(posts[i]['data']['title'])
-        else:
-            print("None")
-    except (requests.RequestException, KeyError, ValueError):
-        print("None")
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
