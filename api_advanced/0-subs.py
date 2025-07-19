710aca5c4ede357a7417e8a7c368822ab400c072
Chomba <clarenchomba59@gmail.com>
2025-07-19 14:58:36 +0000

    Refactor module structure by removing the number_of_subscribers function and updating the main execution block for clarity

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python3
 """
-Module that contains function to get number of subscribers for a subreddit
+0-main
 """
-import requests
+import sys
 
-
-def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API and returns the number of subscribers
-    for a given subreddit. If an invalid subreddit is given, return 0.
-    """
-    if not subreddit or not isinstance(subreddit, str):
-        return 0
-    
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'python:subreddit.subscriber.counter:v1.0 (by /u/pythonbot)'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except Exception:
-        return 0
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
