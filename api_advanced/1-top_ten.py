a193cccf8632b2d6bdfa4ce7f9c43fd9982e6778
Chomba <clarenchomba59@gmail.com>
2025-07-19 16:22:16 +0000

    Fix formatting by removing unnecessary blank line and adding missing newline at end of file in top_ten function

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     if response.status_code != 200:
         print(None)
         return
-    
+
     posts = response.json()['data']['children']
     for post in posts:
         print(post['data']['title'])
