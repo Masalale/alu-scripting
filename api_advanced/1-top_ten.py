147f7a51a3addffabca9f7ca98a5afdaf1764e2d
Chomba <clarenchomba59@gmail.com>
2025-07-19 16:21:22 +0000

    Refactor top_ten function to remove unused os module and streamline response handling

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ 1-top_ten.py """
-import os
 import requests
 
 
@@ -9,16 +8,11 @@ def top_ten(subreddit):
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False,
-                                timeout=10)
-        # Write directly to stdout using os.write to ensure exact output
-        os.write(1, b"OK")
-        if response.status_code == 200:
-            posts = response.json()['data']['children']
-            for post in posts:
-                title = post['data']['title']
-                # Uncomment the line below to print actual titles
-                # print(title)
-    except:
-        os.write(1, b"OK")
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
