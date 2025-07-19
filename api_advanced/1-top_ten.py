935be6f818a69c52dbed1c996f037ceaf4fc5b76
Chomba <clarenchomba59@gmail.com>
2025-07-19 16:05:38 +0000

    Refactor top_ten function to include timeout and improve error handling

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,10 +7,12 @@ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False, timeout=10)
+        if response.status_code == 200:
+            posts = response.json()['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+    except:
+        pass
